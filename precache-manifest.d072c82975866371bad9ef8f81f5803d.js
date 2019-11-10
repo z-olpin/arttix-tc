67e6ed82369c5042ae8c8962566b6c211b21c937
Zachary Olpin <info@zacharyolp.in>
2019-11-09 23:30:40 -0700

    Updates

---
+++ precache-manifest.d072c82975866371bad9ef8f81f5803d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e17e80a6ea5ab16e49b372b8fc21b1c",
+    "url": "/timecard-converter/index.html"
+  },
+  {
+    "revision": "74b1bb4e25264a2dbd48",
+    "url": "/timecard-converter/static/css/main.cf60f4b9.chunk.css"
+  },
+  {
+    "revision": "cbdf2f37802e66c85e17",
+    "url": "/timecard-converter/static/js/2.57a7d0fd.chunk.js"
+  },
+  {
+    "revision": "74b1bb4e25264a2dbd48",
+    "url": "/timecard-converter/static/js/main.dc51eca3.chunk.js"
+  },
+  {
+    "revision": "b0e065c465d4fcf0cd4e",
+    "url": "/timecard-converter/static/js/runtime-main.a153445a.js"
+  }
+]);
