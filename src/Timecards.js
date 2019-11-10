27aa7d97be5bd0201b1ed01b35d176c90d8a8ce4
Zachary Olpin <info@zacharyolp.in>
2019-11-09 21:35:33 -0700

    calculates shift duration and rounds to quarter hours

--- src/Timecards.js
+++ src/Timecards.js
@@ -1,109 +1,110 @@
-import React, {useState} from 'react'
+import React, { useState, useEffect } from 'react'
+import './Timecards.css'
+import cc from "./cc.png";
 
 // TODO: Use only minutes.
 // Fix bug adding 12 hours to  12pm.
 
 const TimeCards = () => {
-const [inHour, setInHour] = useState()
-const [inMin, setInMin] = useState()  
-const [outHour, setOutHour] = useState()
-const [outMin, setOutMin] = useState()  
-const [total, setTotal] = useState()
+  const [inHour, setInHour] = useState(null)
+  const [inMin, setInMin] = useState(null)
+  const [outHour, setOutHour] = useState(null)
+  const [outMin, setOutMin] = useState(null)
+  const [timeIn, setTimeIn] = useState(null)
+  const [timeOut, setTimeOut] = useState(null)
+  const [diff, setDiff] = useState(0)
 
-const inHourChange = e => {
-  const iH = e.target.value
-  setInHour(iH)
+  useEffect(() => {
+    setTimeIn(inHour + inMin)
+  }, [inHour, inMin])
 
-}
+  useEffect(() => {
+    setTimeOut(outHour + outMin)
+  }, [outHour, outMin])
 
-const inMinChange = e => {
-  const iM = e.target.value
-  setInMin(iM)
-}
-
-const outHourChange = e => {
- const oH = e.target.value
- setOutHour(oH)
-}
-
-const outMinChange = e => {
-const oM = e.target.value
-setOutMin(oM)
-
-// TODO: subtract or add minutes, shape to quartiles.
-setTotal(outHour - inHour)
-
-
-}
-
-
+  useEffect(() => {
+    let _diff = timeOut - timeIn
+    let diffHours = Math.trunc(_diff / 60)
+    let diffMinutes = _diff % 60
+    let diffQuartile = Math.round(diffMinutes / 15) * 25
+    setDiff(Number(`${diffHours}.${diffQuartile}`))
+  }, [timeIn, timeOut])
 
   return (
     <>
-    <h1>Time in:</h1>
-      <select onChange={inHourChange}>
+      <main>
+        <div id="select-wrapper">
+        <div className="time-select">
+          <label>IN</label>
+          <select onChange={e => setInHour(Number(e.target.value))}>
+            {[
+              ['6', 360], ['7', 420],
+              ['8', 480], ['9', 540],
+              ['10', 600], ['11', 660],
+              ['12', 720], ['1', 780],
+              ['2', 840], ['3', 900],
+              ['4', 960], ['5', 1020],
+              ['6', 1080], ['7', 1140],
+              ['8', 1200], ['9', 1260]
+            ].map(h => <option value={h[1]}>{h[0]}</option>)}
 
-        {[
-          '5a', '6a', '7a', '8a',
-          '9a', '10a', '11a', '12p',
-          '1p', '2p', '3p', '4p',
-          '5p', '6p', '7p', '8p',
-          '9p', '10p'
-          ].map(h => <option value={(h[h.length-1] === 'p') ? Number(h.slice(0,h.length-1))+12 : Number(h.slice(0,h.length-1))}>{h + 'm'}</option>)}
+          </select>
+          :
+          <select onChange={e => setInMin(Number(e.target.value))}>
+            {[
+              0, 1, 2, 3, 4, 5, 6,
+              7, 8, 9, 10, 11, 12, 13,
+              14, 15, 16, 17, 18, 19, 20,
+              21, 22, 23, 24, 25, 26, 27,
+              28, 29, 30, 31, 32, 33, 34,
+              35, 36, 37, 38, 39, 40, 41,
+              42, 43, 44, 45, 46, 47, 48,
+              49, 50, 51, 52, 53, 54, 55,
+              56, 57, 58, 59
+            ].map(m => <option value={m}>{m.toString().padStart(2, 0)}</option>)}
 
-      </select>
-      <select onChange={inMinChange}>
-        {[
-          '00', '01', '02', '03', '04',
-          '05', '06', '07', '08', '09',
-          '10', '11', '12', '13', '14',
-          '15', '16', '17', '18', '19',
-          '20', '21', '22', '23', '24',
-          '25', '26', '27', '28', '29',
-          '30', '31', '32', '33', '34',
-          '35', '36', '37', '38', '39',
-          '40', '41', '42', '43', '44',
-          '45', '46', '47', '48', '49',
-          '50', '51', '52', '53', '54',
-          '55', '56', '57', '58', '59'
-        ].map(m => <option value={Number(m)}>{m}</option>)}
+          </select>
+        </div>
+        <div className="time-select">
+          <label>OUT</label>
+          <select onChange={e => setOutHour(Number(e.target.value))}>
+            {[
+              ['6', 360], ['7', 420],
+              ['8', 480], ['9', 540],
+              ['10', 600], ['11', 660],
+              ['12', 720], ['1', 780],
+              ['2', 840], ['3', 900],
+              ['4', 960], ['5', 1020],
+              ['6', 1080], ['7', 1140],
+              ['8', 1200], ['9', 1260],
+              ['10', 1320]
+            ].map(h => <option value={h[1]}>{h[0]}</option>)}
 
-      </select>
+          </select>
+          :
+          <select onChange={e => setOutMin(Number(e.target.value))}>
+            {[
+              0, 1, 2, 3, 4, 5, 6,
+              7, 8, 9, 10, 11, 12, 13,
+              14, 15, 16, 17, 18, 19, 20,
+              21, 22, 23, 24, 25, 26, 27,
+              28, 29, 30, 31, 32, 33, 34,
+              35, 36, 37, 38, 39, 40, 41,
+              42, 43, 44, 45, 46, 47, 48,
+              49, 50, 51, 52, 53, 54, 55,
+              56, 57, 58, 59
+            ].map(m => <option value={m}>{m.toString().padStart(2, 0)}</option>)}
 
-      <h1>Time out:</h1>
+          </select>
+        </div>
+        </div>
 
-      <select onChange={outHourChange}>
-
-        {[
-          '5a', '6a', '7a', '8a',
-          '9a', '10a', '11a', '12p',
-          '1p', '2p', '3p', '4p',
-          '5p', '6p', '7p', '8p',
-          '9p', '10p'
-          ].map(h => <option value={(h[h.length-1] === 'p') ? Number(h.slice(0,h.length-1))+ 12 : Number(h.slice(0,h.length-1))}>{h + 'm'}</option>)}
-
-      </select>
-      <select onChange={outMinChange}>
-        {[
-          '00', '01', '02', '03', '04',
-          '05', '06', '07', '08', '09',
-          '10', '11', '12', '13', '14',
-          '15', '16', '17', '18', '19',
-          '20', '21', '22', '23', '24',
-          '25', '26', '27', '28', '29',
-          '30', '31', '32', '33', '34',
-          '35', '36', '37', '38', '39',
-          '40', '41', '42', '43', '44',
-          '45', '46', '47', '48', '49',
-          '50', '51', '52', '53', '54',
-          '55', '56', '57', '58', '59'
-        ].map(m => <option value={Number(m)}>{m}</option>)}
-
-      </select>
-
-      
-      {total &&
-              <h1>Just the hour diff: {total}</h1>}
+        <h2>Total hours: {diff}</h2>
+      </main>
+      <footer>
+        <img id="cc" src={cc} width='48px' alt="CC license"></img>
+        <a id="repo" href="http://github.com/vxxce/timecard-converter">Zachary Olpin</a>
+       </footer>
     </>
   )
 }
--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import ReactDOM from 'react-dom';
 import TimeCards from './Timecards';
 
 
+
 ReactDOM.render(<TimeCards />, document.getElementById('root'));
