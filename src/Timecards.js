bcf7ec890bef176ff9ca2d4b3a4308b96937b39d
Zachary Olpin <info@zacharyolp.in>
2019-11-13 15:31:56 -0700

    test

--- src/Timecards.js
+++ src/Timecards.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import './Timecards.css'
 import cc from "./cc.png";
+//test
 
 const TimeCards = () => {
   const [inHour, setInHour] = useState()
