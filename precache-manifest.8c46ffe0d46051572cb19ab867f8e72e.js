d65036704c6d8db48cda96ea313e8461d76fc3ad
Zachary Olpin <info@zacharyolp.in>
2019-11-15 22:54:25 -0700

    Updates

--- precache-manifest.8c46ffe0d46051572cb19ab867f8e72e.js
+++ precache-manifest.8c46ffe0d46051572cb19ab867f8e72e.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "41f6c098c5f3082304084021350ff4c1",
+    "revision": "4cc7ee2dad75316e3cc4ff61299d74e0",
     "url": "/arttix-tc/index.html"
   },
   {
-    "revision": "e88f8dd52cad6af743b9",
+    "revision": "fde54f53b649446a99df",
     "url": "/arttix-tc/static/css/main.2355072f.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/arttix-tc/static/js/2.57a7d0fd.chunk.js"
   },
   {
-    "revision": "e88f8dd52cad6af743b9",
-    "url": "/arttix-tc/static/js/main.a86a66d0.chunk.js"
+    "revision": "fde54f53b649446a99df",
+    "url": "/arttix-tc/static/js/main.660a9e34.chunk.js"
   },
   {
     "revision": "addbffff18512451bcb5",
